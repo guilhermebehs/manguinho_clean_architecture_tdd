edeb66dda14fd6a629c27a8a40161591550f3a9a
Guilherme <guilhermebehs2013@hotmail.com>
2021-07-21 11:45:45 -0300

    feat: ensure SignUpController calls AddAccount with correct values

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,11 @@
+import { AddAccount } from './../../domain/usecases/add-account'
 import { InvalidParamError, MissingParamError } from './../errors'
 import { EmailValidator, Controller, HttpRequest, HttpResponse } from './../protocols'
 import { badRequest, serverError } from './../helpers/http-helper'
 export class SignUpController implements Controller {
-  constructor (private readonly emailValidator: EmailValidator) {}
+  constructor (
+    private readonly emailValidator: EmailValidator,
+    private readonly addAccount: AddAccount) {}
 
   handle (httpRequest: HttpRequest): HttpResponse {
     try {
@@ -10,11 +13,12 @@ export class SignUpController implements Controller {
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) { return badRequest(new MissingParamError(field)) }
       }
-      const { email, password, passwordConfirmation } = httpRequest.body
+      const { name, email, password, passwordConfirmation } = httpRequest.body
       if (password !== passwordConfirmation) { return badRequest(new InvalidParamError('passwordConfirmation')) }
 
       if (!this.emailIsValid(email)) { return badRequest(new InvalidParamError('email')) }
 
+      this.addAccount.add({ name, email, password })
       return { statusCode: 0, body: null }
     } catch (e) {
       return serverError()
