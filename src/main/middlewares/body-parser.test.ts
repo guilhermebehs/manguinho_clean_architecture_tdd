b1e5f108251f42d44e6628220fded61811f98f4c
Guilherme <guilhermebehs2013@hotmail.com>
2021-07-29 17:13:29 -0300

    feat: ensure requests parse body as json

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,13 @@
+import request from 'supertest'
+import app from '../config/app'
+describe('Body Parser', () => {
+  test('', async () => {
+    app.post('/test_body_parser', (req, res) => {
+      res.send(req.body)
+    })
+    await request(app)
+      .post('/test_body_parser')
+      .send({ name: 'Guilherme' })
+      .expect({ name: 'Guilherme' })
+  })
+})
