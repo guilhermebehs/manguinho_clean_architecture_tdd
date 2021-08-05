c127040e2fbe31466e1ce65f87fecebae525cd82
Guilherme <guilhermebehs2013@hotmail.com>
2021-08-05 14:02:12 -0300

    feat: ensure DbAuthentication calls UpdateAccessTokenRepository with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,3 +1,4 @@
+import { UpdateAccessTokenRepository } from './../../protocols/db/update-access-token-repository'
 import { TokenGenerator } from './../../protocols/criptography/token-generator'
 import { HashComparer } from './../../protocols/criptography/hash-comparer'
 import { LoadAccountByEmailRepository } from './../../protocols/db/load-account-by-email-repository'
@@ -7,7 +8,8 @@ export class DbAuthentication implements Authentication {
   constructor (
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
     private readonly hashComparer: HashComparer,
-    private readonly tokenGeneratorStub: TokenGenerator
+    private readonly tokenGenerator: TokenGenerator,
+    private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
   ) {}
 
   async auth (authentication: AuthenticationModel): Promise<string | null> {
@@ -16,7 +18,8 @@ export class DbAuthentication implements Authentication {
     if (account) {
       const isValid = await this.hashComparer.compare(password, account.password)
       if (isValid) {
-        const accessToken = await this.tokenGeneratorStub.generate(account.id)
+        const accessToken = await this.tokenGenerator.generate(account.id)
+        await this.updateAccessTokenRepository.update(account.id, accessToken)
         return accessToken
       }
     }
