3a7e0cbfc99d4691e10797ad78c888254281555b
Guilherme <guilhermebehs2013@hotmail.com>
2021-08-13 17:11:19 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -46,5 +46,14 @@ describe('Login Routes', () => {
           .expect(201)
       })
     })
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'guilhermebehs2013@hotmail.com',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })
