47ccb766f7d81543950ee108020fe26e970ced5d
Guilherme <guilhermebehs2013@hotmail.com>
2021-08-18 18:14:50 -0300

    feat: ensure AccountMongoRepository find an account by token with role

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -14,7 +14,7 @@ export class AccountMongoRepository implements AddAccountRepository, LoadAccount
 
   async loadByToken (token: string, role?: string): Promise<AccountModel | null> {
     const accountCollection = await MongoHelper.getCollection('accounts')
-    const result = await accountCollection.findOne({ token })
+    const result = await accountCollection.findOne({ token, role })
     return result && MongoHelper.map(result)
   }
 
