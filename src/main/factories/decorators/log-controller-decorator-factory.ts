e096dc021ca57a236560e5b28203b76ea9b0711f
Guilherme <guilhermebehs2013@hotmail.com>
2021-08-18 08:44:10 -0300

    refactor: separate factories in folders

---
+++ src/main/factories/decorators/log-controller-decorator-factory.ts
@@ -0,0 +1,7 @@
+import { LogControllerDecorator } from './../../decorators/log-controller-decorator'
+import { LogMongoRepository } from './../../../infra/db/mongodb/log/log-mongo-repository'
+import { Controller } from './../../../presentation/protocols/controller'
+export const makeLogControllerDecorator = (controller: Controller): Controller => {
+  const logErrorRepository = new LogMongoRepository()
+  return new LogControllerDecorator(controller, logErrorRepository)
+}
