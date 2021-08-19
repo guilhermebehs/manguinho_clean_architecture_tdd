83a58c6520ba10a5c2aec2d9c0bd7bca360465c8
Guilherme <guilhermebehs2013@hotmail.com>
2021-08-19 14:24:25 -0300

    feat: ensure LoadSurveysController calls LoadSurveys

---
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
@@ -0,0 +1,49 @@
+import { SurveyModel, LoadSurveys } from './load-surveys-controller-protocols'
+import { LoadSurveysController } from './load-surveys-controller'
+import Mockdate from 'mockdate'
+
+const makeFakeSurveys = (): SurveyModel[] => ([
+  {
+    id: 'any_id',
+    question: 'any_question',
+    answers: [
+      {
+        image: 'any_image',
+        answer: 'any_answer'
+      }
+    ],
+    date: new Date()
+  },
+  {
+    id: 'other_id',
+    question: 'other_question',
+    answers: [
+      {
+        image: 'other_image',
+        answer: 'other_answer'
+      }
+    ],
+    date: new Date()
+  }
+])
+
+describe('LoadSurveys Controller', () => {
+  beforeAll(() => {
+    Mockdate.set(new Date())
+  })
+  afterAll(() => {
+    Mockdate.reset()
+  })
+  test('Should call Load', async () => {
+    class LoadSurveysStub implements LoadSurveys {
+      async load (): Promise<SurveyModel[]> {
+        return new Promise(resolve => resolve(makeFakeSurveys()))
+      }
+    }
+    const loadSurveysStub = new LoadSurveysStub()
+    const loadSpy = jest.spyOn(loadSurveysStub, 'load')
+    const sut = new LoadSurveysController(loadSurveysStub)
+    await sut.handle({})
+    expect(loadSpy).toHaveBeenCalled()
+  })
+})
