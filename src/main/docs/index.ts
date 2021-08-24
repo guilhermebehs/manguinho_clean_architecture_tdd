961baa0bddab1ca3e1cad23fa61cba4ea1998296
Guilherme <guilhermebehs2013@hotmail.com>
2021-08-24 18:05:39 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    description: 'Api do curso do Mango para realizer enquetes entre desenvolvedores',
+    version: '1.0.0'
+  }
+}
