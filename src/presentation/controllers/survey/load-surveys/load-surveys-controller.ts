2dada8d6c9ccef2a78480ac0ac224c34867f626e
Guilherme <guilhermebehs2013@hotmail.com>
2021-08-29 10:59:57 -0300

    feat: change load surveys to add didAnswer field

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -5,7 +5,8 @@ export class LoadSurveysController implements Controller {
   constructor (private readonly loadSurveys: LoadSurveys) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const surveys = await this.loadSurveys.load()
+      const { accountId } = httpRequest
+      const surveys = await this.loadSurveys.load(accountId as string)
       return surveys.length ? ok(surveys) : noContent()
     } catch (error) {
       return serverError(error)
