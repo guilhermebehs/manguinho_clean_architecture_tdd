e4100505221896813f85f0ec2c693177fe588e0e
Guilherme <guilhermebehs2013@hotmail.com>
2021-08-31 16:12:40 -0300

    feat: setup apllo server

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -3,8 +3,10 @@ import setupMiddlewares from './middlewares'
 import setupStaticFiles from './static-files'
 import setupRoutes from './routes'
 import setupSwagger from './swagger'
+import setupApolloServer from './apollo-server'
 
 const app = express()
+setupApolloServer(app)
 setupSwagger(app)
 setupStaticFiles(app)
 setupMiddlewares(app)
