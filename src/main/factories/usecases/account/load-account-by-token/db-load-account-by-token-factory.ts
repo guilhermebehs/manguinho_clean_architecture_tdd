a3d94aefdeb42cde18bb3b8baaef57c2464138cb
Guilherme <guilhermebehs2013@hotmail.com>
2021-09-01 14:13:03 -0300

    feat: add survey result schema

--- src/main/factories/usecases/account/load-account-by-token/db-load-account-by-token-factory.ts
+++ src/main/factories/usecases/account/load-account-by-token/db-load-account-by-token-factory.ts
@@ -2,7 +2,7 @@ import { LoadAccountByToken } from '@/domain/usecases/account/load-account-by-to
 import { DbLoadAccountByToken } from '@/data/usecases/account/load-account-by-token/db-load-account-by-token'
 import { JwtAdapter } from '@/infra/criptography/jwt-adapter/jwt-adapter'
 import { AccountMongoRepository } from '@/infra/db/mongodb/account/account-mongo-repository'
-import env from '../../../../config/env'
+import env from '@/main/config/env'
 
 export const makeDbLoadAccountByToken = (): LoadAccountByToken => {
   const accountMongoRepository = new AccountMongoRepository()
